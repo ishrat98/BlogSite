d621e512ef6c6060fc51ef10c1926108cac66ea8
emu <bsse0927@iit.du.ac.bd>
2020-02-25 15:50:04 +0600

    error

--- src/app/blog-list/blog-list.component.ts
+++ src/app/blog-list/blog-list.component.ts
@@ -8,10 +8,14 @@ import { BlogPost } from '../Blog-post';
 })
 export class BlogListComponent implements OnInit {
 
-  blogPosts: BlogPost[];
+  blogPosts: BlogPost[] = [];
   constructor() { }
 
   ngOnInit() {
+    this.blogPosts.push(new BlogPost('Blog Post 1', 'Summary 1'));
+    this.blogPosts.push(new BlogPost('Blog Post 2', 'Summary 2'));
+    this.blogPosts.push(new BlogPost('Blog Post 3', 'Lorem ipsum dolor sit ame?'));
+
   }
 
 }
--- src/app/blog-post-tile/blog-post-tile.component.ts
+++ src/app/blog-post-tile/blog-post-tile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { BlogPost } from '../Blog-post';
+import { TruncatePipe } from '../truncate.pipe';
 
 @Component({
   selector: 'app-blog-post-tile',
@@ -7,13 +8,21 @@ import { BlogPost } from '../Blog-post';
   styleUrls: ['./blog-post-tile.component.css']
 })
 export class BlogPostTileComponent implements OnInit {
-    @Input() Title: string;
-    @Input() summary: string;
   @Input() post: BlogPost;
 
-  constructor() { }
+  fullSummary: string;
+
+  constructor(private truncatePipe: TruncatePipe) { }
 
   ngOnInit() {
+    this.fullSummary = this.post.summary;
+
+    this.post.summary = this.truncatePipe.transform(this.post.summary, 30);
+
+  }
+
+  showFullSummary() {
+    this.post.summary = this.fullSummary;
 
   }
 

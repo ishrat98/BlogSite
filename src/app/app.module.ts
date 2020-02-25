92edce6cc6323e0faf79b671a926dc82f14ed9a0
emu <bsse0927@iit.du.ac.bd>
2020-02-25 12:23:49 +0600

    upto bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogPostTileComponent } from './blog-post-tile/blog-post-tile.component';
+import { BlogListComponent } from './blog-list/blog-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogPostTileComponent,
+    BlogListComponent
   ],
   imports: [
     BrowserModule,
